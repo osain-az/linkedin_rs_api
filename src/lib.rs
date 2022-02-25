b1ba73edd5cabb8140c5de0130d06a18710ab808
azibodusi osain <azibodusiosain@gmail.com>
2022-02-25 12:18:59 +0100

    feature: added different posting method

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
-mod login;
 pub mod account;
 pub mod client;
+pub mod http_clinent;
+mod login;
+pub mod share;
 
 pub mod prelude {
-    pub use crate::login::prelude::*;
     pub use crate::client::Client;
-
+    pub use crate::login::prelude::*;
+    pub use crate::share::post::*;
 }
-
---
+++ src/share/mod.rs
@@ -0,0 +1,3 @@
+pub mod post;
+pub mod srtuct_helper;
+mod utils;
